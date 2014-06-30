e1792b9c948b563fe3ed310d9e0558bb31d736e5
alnutile <alfrednutile@gmail.com>
2014-06-30 11:21:34 -0400

    Fix migration left over issues

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder {
         if(Config::get('database.default') != 'sqlite') {
             DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         }
-        DB::table('users')->truncate();
-        $this->call('UserTableSeeder');
+        //DB::table('users')->truncate();
+        //$this->call('UserTableSeeder');
 
         if(Config::get('database.default') != 'sqlite') {
             DB::statement('SET FOREIGN_KEY_CHECKS=1;');
